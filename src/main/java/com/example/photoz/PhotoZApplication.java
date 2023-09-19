1329827abe75cff7adcb893c799fc4bdc45b66ce
Ionut2707 <ionut.dima2707@gmail.com>
2023-09-19 19:56:57 +0300

    Initial commit

---
+++ src/main/java/com/example/photoz/PhotoZApplication.java
@@ -0,0 +1,13 @@
+package com.example.photoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoZApplication.class, args);
+	}
+
+}
